8c3040e45443394e45d04721e0fd1319b293f079
AndriMarKristjansson <andrik25@ru.is>
2025-12-01 16:24:01 +0000

    Added python path 8 and 9

--- designDocument/lowfi_happyPath_python/8.spectate.py
+++ designDocument/lowfi_happyPath_python/8.spectate.py
@@ -1,29 +1,65 @@
-'''8. You want to log in as a spectator and go to view the results of the MuppetsVsDocHopper tournament'''
+'''8. You want to log in as a spectator and go to see who won the MuppetsVsDocHopper tournament'''
 
 
-print('''''')
-
-a = input()
-print()
-print()
-
-
-print('''''')
-
-a = input()
-print()
-print()
-
-
-print('''''')
+print('''Main Menu
+—————————
+1 Login
+2 Register 
+3 Spectate
+q Quit
+—————————
+Choose Action:
+—————————''')
 
 a = input()
 print()
 print()
 
 
-print('''''')
+print('''Spectate Page
+—————————
+1 Players
+2 Clubs
+3 Teams
+4 Tournaments
+b Back
+—————————
+Choose Action:''')
 
 a = input()
 print()
 print()
+
+
+print('''Tournaments
+——————————————————
+~~~~~~, active
+TheMuppetsVsDocHopper, archived
+——————————————————
+b Back
+Select Tournament:
+——————————————————''')
+
+a = input()
+print()
+print()
+
+
+print('''Archived Tournament
+——————————————————
+- - -TheMuppetsVsDocHopper- - -
+
+Tournament date: 2025 11 27 - 2025 11 29
+Rounds Played: 30
+Winner: TheMuppets
+——————————————————
+1 Game Schedule
+2 Teams
+3 Brackets
+b Back
+——————————————————''')
+
+a = input()
+print()
+print()
+
