289b1e22d4e6b9d5027d0063d3ccc286bc43b2e2
AndriMarKristjansson <andrik25@ru.is>
2025-12-02 13:37:56 +0000

    created a breadcrumb example in python

---
+++ designDocument/lowfi_happyPath_python/backtrack_example.py
@@ -0,0 +1,74 @@
+print()
+a = 0
+while a != "q":
+    print("\n" + '''Admin Page [b ap] | Manage Tournaments [b mt] | Manage Unactive Tournament [b mut]
+    ————————————————————————————————————————————————————————————————————————————
+                                    Edit Tournament
+    ————————————————————————————————————————————————————————————————————————————
+    - - - - - - - - - - - - - -  - -Tournament Name- - - - - - - - - - - - - - - - -
+
+    Date: yyyy mm dd - yyyy mm dd
+    Venue: 123 placename, cityname
+    Contact Info:
+        Email: somth@somth.com
+        Phone number: 1234567
+
+    ————————————————————————————————————————————————————————————————————————————
+    1 Edit Time / Date
+    2 Edit Info
+    b Back
+    ————————————————————————————————————————————————————————————————————————————
+    Choose Action:
+    ————————————————————————————————————————————————————————————————————————————''')
+
+
+    a = input("    ")
+
+
+    if a == "b ap":
+        print("\n" + '''Admin Page
+    —————————————
+    1 Create Tournament
+    2 Manage Tournaments
+    3 Create Club
+    b Back
+    —————————————
+    Choose Action:
+    —————————————''')
+        a = input("    ")
+
+    elif a == "b mt":
+        print("\n" + '''Manage Tournaments
+——————————————————
+- - - -List Of Tournaments - - - -
+
+~~~~~~~~~~~
+~~~~~~~~~~~
+~~~~~~~~~~~
+TheMuppetsVsDocHopper
+~~~~~~~~~~~
+~~~~~~~~~~~
+——————————————————
+Choose A Tournament To Manage:''')
+        a = input("    ")
+
+
+    elif a == "b" or a == "b mut":
+        print("\n" + '''Manage Unactive Tournaments
+——————————————————
+- - -TheMuppetsVsDocHopper- - -
+
+1 Manage Teams
+2 Publish
+3 Edit Tournament
+b Back
+——————————————————
+Choose Action:
+——————————————————''')
+        
+        a = input("    ")
+
+
+
+
+
