0209acf5e06ad5ac00aa4643dd47ed5be51b1dc4
kristjanhj <kristjanhagalin@gmail.com>
2025-12-03 11:37:56 +0000

    Added __init__.py for Models

---
+++ Project/Models/__init__.py
@@ -0,0 +1,15 @@
+from Club import Club
+from Match import Match
+from Player import Player
+from Server import Server
+from Team import Team
+from Tournament import Tournament
+
+__all__ = [
+    "Club",
+    "Match",
+    "Player",
+    "Server",
+    "Team",
+    "Tournament",
+]
