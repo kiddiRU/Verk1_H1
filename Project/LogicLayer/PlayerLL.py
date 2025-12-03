ceecc2e3d0c37f828a20833fdbe4ec4d9b08a478
Elmar-Sig <elmar.sigmars@gmail.com>
2025-12-03 10:31:09 +0000

    Created PlayerLL and listed all functions in the file

---
+++ Project/LogicLayer/PlayerLL.py
@@ -0,0 +1,56 @@
+"""
+Author: Elmar Sigmarsson
+Date: 2025-12-03
+
+BLa bla
+"""
+
+class PlayerLL():
+    def __init__(self):
+        pass
+
+
+    """info from player to validate"""
+
+    #TODO call a file that checks validations for name
+    def validate_name():
+        pass
+
+    #TODO call a file that checks validations for date of birth
+    def validate_date_of_birth():
+        pass
+
+    #TODO call a file that checks validations for name
+    def validate_home_address():
+        pass
+    
+    #TODO call a file that checks validations for name
+    def validate_email():
+        pass
+    
+    #TODO call a file that checks validations for name
+    def validate_phone_number():
+        pass
+    
+    #TODO call a file that checks validations for name
+    def validate_handle(): #Unique
+        pass
+
+    
+    """functions for player"""
+
+    #TODO implement creating a player  
+    def create_player(self):
+        pass
+
+    #TODO implement changing player info
+    def change_player_info(self):
+        pass
+
+    #TODO implement creating a team
+    def create_team():
+        pass
+
+    #TODO implement leaving a team
+    def leave_team():
+        pass
