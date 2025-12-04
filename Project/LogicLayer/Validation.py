62fcc9a792e451aa0f8c468aed65ad9c6accaeea
Elmar-Sig <elmar.sigmars@gmail.com>
2025-12-04 11:18:49 +0000

    added functionality to validate a date

--- Project/LogicLayer/Validation.py
+++ Project/LogicLayer/Validation.py
@@ -7,41 +7,46 @@ A validation file that takes inn all info that would need to be validated
 
 from Models.Exception import ValidationError
 from DataLayer import DataLayerAPI
+from datetime import date
 
 
-def validate_unique_name(unique_name: str, type_of_name: str) -> None:
+def validate_unique_name(unique_name: str, type_of_name: str) -> str | ValidationError:
     """
     Checks if the name is unique and is between 3-40 char in length
     Used for unique player handle, team tournament and club names
     """
 
-    if len(unique_name) >= 3 and len(unique_name) <= 40:
+    if len(unique_name) < 3 and len(unique_name) > 40:
         raise ValidationError("Name needs to be between 3 to 40 characters in length")
-    
+        
     if type_of_name is "PLAYER":
         model_player: list = DataLayerAPI.load_players()
 
         for player in model_player:
             if player.name == unique_name:
                 raise ValidationError("Name is already taken")
-
+        
+        return unique_name
 
     #TODO implement check for unique names in team, tournament and club
-    if type_of_name is "TEAM":
+    elif type_of_name is "TEAM":
         pass
 
-    if type_of_name is "TOURNAMENT":
+    elif type_of_name is "TOURNAMENT":
         pass
 
-    if type_of_name is "CLUB":
+    elif type_of_name is "CLUB":
         pass
 
 
-def validate_name(name) -> None: # Players full name
+def validate_name(name) -> str | ValidationError: # Players full name
     """Checks if the name is in between 3-40 char in length and has only letters"""
     
-    if len(name) >= 3 and len(name) <= 40:
+    if len(name) < 3 and len(name) > 40:
         raise ValidationError("Name needs to be between 3 to 40 characters in length")
+    
+    else:
+        return name
     
     
 def validate_home_address(home_address) -> bool: # Players home address
@@ -57,9 +62,21 @@ def validate_email(email) -> bool: # Players and tournament contact email
     """Checks if email has @, something before the @ and behind it and .XX in the end (johndoe@gmail.com)"""
     return email
 
-def validate_date(date) -> bool: # Date of Birth, Date of Tournament
+def validate_date(date_input) -> str | ValidationError: # Date of Birth, Date of Tournament
     """Checks if date format is correct YYYY-MM-DD"""
-    return date
+
+    date_list = date_input.split("-")
+    year: int = int(date_list[0])
+    month: int =  int(date_list[1])
+    day: int = int(date_list[2])
+
+    try:
+        validate_date = date(year, month, day)
+        return str(validate_date)
+    
+    except:
+        raise ValidationError("Date inputted incorrectly")
+
 
 def validate_date_frame(date_1, date_2) -> bool: # Date frame of tournament
     """Checks if date frame is correct, date_1 is before date_2 (2025-12-01 -> 2025-12-06)"""
