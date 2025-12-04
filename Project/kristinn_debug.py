01a395fe9b70452503b56fa12b86794cbd9321c4
kristjanhj <kristjanhagalin@gmail.com>
2025-12-04 09:26:15 +0000

    'create_player' testing

--- Project/kristinn_debug.py
+++ Project/kristinn_debug.py
@@ -1,26 +1,31 @@
-from Models.Player import Player
+from Models import Player
 import DataLayer.DataLayerAPI as dlapi
 
-from LogicLayer import LogicLayerAPI
-from LogicLayer import PlayerLL
+from LogicLayer import LogicLayerAPI, PlayerLL
 
 from uuid import uuid4
 
 uuid = str(uuid4())
 
-p = Player(uuid,
-           "Kristinn Hrafn",
-           "2006-07-13",
-           "hraunbraut 43",
-           "kristinnd25@ru.is",
-           "855-2006",
-           "kiddi",
-           "",)
+for p in dlapi.load_players():
+    print(p.name)
 
+# Store player með data layer
+#
+# p = Player(uuid,
+#            "Kristinn Hrafn",
+#            "2006-07-13",
+#            "hraunbraut 43",
+#            "kristinnd25@ru.is",
+#            "855-2006",
+#            "kiddi",
+#            "",)
 # dlapi.store_player(p)
-
 # dlapi.update_player(uuid, "home_address", "brog 36")
 
+
+# Bua til player i gegnum playerLL
+#
 # p = PlayerLL()
 # p.create_player(
 #     "Kristinn Hrafn",
@@ -32,12 +37,15 @@ p = Player(uuid,
 #     ""
 # )
 
-LogicLayerAPI.create_player(
-    "Kristinn Hrafn",
-    "2006-07-13",
-    "hraunbraut 43",
-    "kristinnd25@ru.is",
-    "855-2006",
-    "kiddi",
-    ""
-)
+
+# Bua til player i gegnum LogicLayerAPI
+#
+# LogicLayerAPI.create_player(
+#     "Kristinn Hrafn",
+#     "2006-07-13",
+#     "hraunbraut 43",
+#     "kristinnd25@ru.is",
+#     "855-2006",
+#     "kiddi",
+#     ""
+# )
