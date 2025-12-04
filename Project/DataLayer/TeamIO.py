1b3b4c6d93376a1a5ae467cd43b8b695d6bfb4a0
kristinnd <krihradan@gmail.com>
2025-12-04 09:56:28 +0000

    no particular edit

--- Project/DataLayer/TeamIO.py
+++ Project/DataLayer/TeamIO.py
@@ -12,7 +12,7 @@ from Models import Team
 FILE_PATH = "./DataLayer/Repository/teams.json"
 
 def store_team(team: Team) -> None:
-    data: dict[str, all] = {
+    data = {
         "name": team.name,
         "list_player_uuid": team.list_player_uuid,
         "team_captain_uuid": team.team_captain_uuid,
@@ -23,7 +23,7 @@ def store_team(team: Team) -> None:
     }
     
     with open(FILE_PATH, "r") as team_file:
-        file_content: dict[str, dict[str, all]] = dict(json.load(team_file))
+        file_content = json.load(team_file)
 
     file_content[team.uuid] = data
 
@@ -32,7 +32,7 @@ def store_team(team: Team) -> None:
 
 def load_teams() -> list[Team]:
     with open(FILE_PATH, "r") as team_file:
-        file_content: dict[str, dict[str, all]] = dict(json.load(player_file))
+        file_content = dict(json.load(team_file))
 
     team_list: list[Team] = []
     for uuid, value in file_content.items():
@@ -47,13 +47,13 @@ def load_teams() -> list[Team]:
 
     return team_list
 
-def update_team(uuid: str, key: str, value: all) -> None:
+def update_team(uuid: str, key: str, value) -> None:
     with open(FILE_PATH, "r") as team_file:
-        file_content: dict[str, dict[str, all]] = dict(json.load(team_file))
+        file_content = dict(json.load(team_file))
 
     if uuid in file_content:
         if key in file_content:
             file_content[uuid][key] = value
 
     with open(FILE_PATH, "w") as team_file:
-        json.dump(file_content, player_file)
+        json.dump(file_content, team_file)
