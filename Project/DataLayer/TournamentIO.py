bf02d7241b7dda917204b530d1a255977e6f73db
kristinnd <krihradan@gmail.com>
2025-12-04 17:04:01 +0000

    started on tournamentIO file

---
+++ Project/DataLayer/TournamentIO.py
@@ -0,0 +1,11 @@
+"""
+Author: Kristinn Hrafn <Kristinnd25@ru.is>
+Date: 2025-12-04
+
+Functions which let you read, write and update tournaments
+stored in the ./DataLayer/Repository/touranment.json
+"""
+
+import json
+from Models import Tournament
+
