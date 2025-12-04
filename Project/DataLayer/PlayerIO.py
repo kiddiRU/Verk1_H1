93eea4e56c4a4558f57d22bdc5bdbe9da9626efb
kristinnd <krihradan@gmail.com>
2025-12-04 14:08:03 +0000

    updated PlayerIO with better read write options

--- Project/DataLayer/PlayerIO.py
+++ Project/DataLayer/PlayerIO.py
@@ -18,21 +18,17 @@ Inserts information about the Player class into a json file
 for storage.
 """
 def store_player(player: Player) -> None:
-    data: dict[str, str] = {
-        "name": player.name, 
-        "date_of_birth": player.date_of_birth,
-        "home_address": player.home_address,
-        "email": player.email,
-        "phone_number": player.phone_number,
-        "handle": player.handle,
-        "url": player.url
-    }
-        
+    # Changes object player into a dictionary mapping keys to attributes.
+    data = player.__dict__
+    
+    # Reads json file containing players and stores it as a dictionary.
     with open(FILE_PATH, "r") as player_file:
-        file_content: dict[str, dict[str, str]] = dict(json.load(player_file))
-
+        file_content = dict(json.load(player_file))
+    
+    # Updates the dictionary adding the new player model into the file content.
     file_content[player.uuid] = data
-        
+    
+    # Writes the updated file content into the file containing players.
     with open(FILE_PATH, "w") as player_file:
         json.dump(file_content, player_file)
 
@@ -46,19 +42,15 @@ Player model objects of each entry in the json file.
 Returns the created player list.
 """
 def load_players() -> list[Player]:
+    # Reads the json file containing players and stores it as a dictionary.
     with open(FILE_PATH, "r") as player_file:
-        file_content: dict[str, dict[str, str]] = dict(json.load(player_file))
-        
+        file_content = dict(json.load(player_file))
+    
+    # Creates a list of all players in the player_file as a Player model object.
     player_list: list[Player] = []
-    for uuid, value in file_content.items():
-        player_list.append(Player(uuid,
-                             value["name"],
-                             value["date_of_birth"],
-                             value["home_address"],
-                             value["email"],
-                             value["phone_number"],
-                             value["handle"],
-                             value["url"],))
+   for value in file_content.values():
+        # Uses **value to unpack the dictionary into a Player model object.
+        player_list.append(Player(**value))
         
     return player_list
        
@@ -71,13 +63,17 @@ Will attempt to find player with given uuid and update the
 value tied to given key of that player
 """
 def update_player(uuid: str, key: str, value: str) -> None:
+    # Reads the json file containing players and stores it as a dictionary.
     with open(FILE_PATH, "r") as player_file:
-        file_content: dict[str, dict[str, str]] = dict(json.load(player_file))
-
+        file_content = dict(json.load(player_file))
+    
+    # Updates the file content, checking if the uuid and key exist in
+    # the dictionary.
     if uuid in file_content:
         if key in file_content[uuid]:
             file_content[uuid][key] = value
 
+    # Wriets the updated dictionary into the player file.
     with open(FILE_PATH, "w") as player_file:
         json.dump(file_content, player_file)
 
