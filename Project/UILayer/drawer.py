e70280e37b0ccd4f8c684f90657e021ec0a5f72d
AndriMarKristjansson <andrik25@ru.is>
2025-12-04 12:50:38 +0000

    emmawatson?

--- Project/UILayer/drawer.py
+++ Project/UILayer/drawer.py
@@ -3,6 +3,9 @@ Author: Andri Már Kristjánsson <andrik25@ru.is>
 Date: 2025-12-04
 
 File that takes in table info, creates the table and can clear the terminal
+
+takes 5 arguments:
+     table name, user path, table info, table options, message to be displayed
 """
 import os
 
@@ -29,7 +32,7 @@ class Drawer():
 
         self.banner_border = REDHIGH
         self.path_color = RED
-        self.table_color = self.BRIGHTER + GREEN
+        self.table_color = self.BRIGHTER + BLUE
 
 
 
@@ -45,24 +48,29 @@ class Drawer():
 
 
         return f"""{self.banner_border}
-————————————————————————————————————————————————————————————————————————————————{self.RESET}{self.BOLD}{self.BRIGHTER}
-                ____  __  __              _____                  __ 
-               / __ \/ / / /   ___       / ___/____  ____  _____/ /_
-              / /_/ / / / /   / _ \______\__ \/ __ \/ __ \/ ___/ __/
-             / _, _/ /_/ /   /  __/_____/__/ / /_/ / /_/ / /  / /_  
-            /_/ |_|\____/    \___/     /____/ .___/\____/_/   \__/  
-                                           /_/                     
-       ______     __                                                               
-      / ____/  __/ /__________ __   ______ _____ _____ _____  ____  ____ _         
-     / __/ | |/ / __/ ___/ __ `/ | / / __ `/ __ `/ __ `/ __ \/_  / / __ `/         
-    / /___ >  </ /_/ /  / /_/ /| |/ / /_/ / /_/ / /_/ / / / / / /_/ /_/ /          
-   /_____/_/|_|\__/_/   \__,_/ |___/\__,_/\__, /\__,_/_/ /_/ /___/\__,_/           
-                                         /____/                             {self.RESET}
+————————————————————————————————————————————————————————————————————————————————{self.RESET}                                           
+            ▗▖ ▗▖▗▄▄▖                 ▗▄▖                          
+            ▐▌ ▐▌▐▛▀▜▌               ▗▛▀▜                 ▐▌       
+            ▐▌ ▐▌▐▌ ▐▌      ▟█▙      ▐▙   ▐▙█▙  ▟█▙  █▟█▌▐███ ▗▟██▖
+            ▐███▌▐███      ▐▙▄▟▌      ▜█▙ ▐▛ ▜▌▐▛ ▜▌ █▘   ▐▌  ▐▙▄▖▘
+            ▐▌ ▐▌▐▌▝█▖     ▐▛▀▀▘ ██▌    ▜▌▐▌ ▐▌▐▌ ▐▌ █    ▐▌   ▀▀█▖
+            ▐▌ ▐▌▐▌ ▐▌     ▝█▄▄▌     ▐▄▄▟▘▐█▄█▘▝█▄█▘ █    ▐▙▄ ▐▄▄▟▌
+            ▝▘ ▝▘▝▘ ▝▀      ▝▀▀       ▀▀▘ ▐▌▀▘  ▝▀▘  ▀     ▀▀  ▀▀▀ 
+                                          ▐▌                                                                     
+                                                                
+            ▗▄▄▄▖                      █                           
+            ▐▛▀▀▘      ▐▌              ▀                           
+            ▐▌   ▝█ █▘▐███  ▟██▖▐▙ ▟▌ ██   ▟█▟▌ ▟██▖▐▙██▖▐███▌ ▟██▖
+            ▐███  ▐█▌  ▐▌   ▘▄▟▌ █ █   █  ▐▛ ▜▌ ▘▄▟▌▐▛ ▐▌  ▗▛  ▘▄▟▌
+            ▐▌    ▗█▖  ▐▌  ▗█▀▜▌ ▜▄▛   █  ▐▌ ▐▌▗█▀▜▌▐▌ ▐▌ ▗▛  ▗█▀▜▌
+            ▐▙▄▄▖ ▟▀▙  ▐▙▄ ▐▙▄█▌ ▐█▌ ▗▄█▄▖▝█▄█▌▐▙▄█▌▐▌ ▐▌▗█▄▄▖▐▙▄█▌
+            ▝▀▀▀▘▝▀ ▀▘  ▀▀  ▀▀▝▘  ▀  ▝▀▀▀▘   ▐▌ ▀▀▝▘▝▘ ▝▘▝▀▀▀▘ ▀▀▝▘
+                                           ▜█▛▘                                          {self.RESET}
 {self.banner_border}————————————————————————————————————————————————————————————————————————————————{self.RESET} \n\n"""
     
 
 
-    def table(self, table_name, table_path = [], table_info = [], table_options = []) -> str:
+    def table(self, table_name, table_path = [], table_info = [], table_options = [], message = "") -> str:
         "Creates and returns the UI tables"
 
         self.clear()
@@ -91,13 +99,19 @@ class Drawer():
 
             table += line
 
+
+        if message:
+            table += message + "\n"
+            table += line
+
+
         if table_options:
             for option in table_options:
                 table += option + "\n"
 
             table += line
             table += "Choose Action:"
-            
+
 
 
         return self.table_color + table + self.RESET
