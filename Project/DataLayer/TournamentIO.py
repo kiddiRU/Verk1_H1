92e585c9503cb86caa140bfe3ba3807c41fb695b
kristinnd <krihradan@gmail.com>
2025-12-05 09:11:51 +0000

    tournamentIO changes

--- Project/DataLayer/TournamentIO.py
+++ Project/DataLayer/TournamentIO.py
@@ -8,4 +8,25 @@ stored in the ./DataLayer/Repository/touranment.json
 
 import json
 from Models import Tournament
+from datetime import date, time
+
+FILE_PATH = "DataLayer/Repository/tournament.json"
+
+def store_tournament(tournament: Tournament) -> None:
+    data = tournament.__dict__
+
+    data["start_date"] = str(data["start_date"])
+    data["end_date"] = str(data["end_date"])
+    data["time_frame_start"] = str(data["time_frame_start"])
+    data["time_frame_end"] = str(data["time_frame_end"])
+    data["status"] = str(data["status"])
+
+    with open(FILE_PATH, "r") as tournament_file:
+        file_content = dict(json.load(tournament_file))
+
+    file_content[tournament.uuid] = data
+
+    with open(FILE_PATH, "w") as tournament_file:
+        json.dump(file_content, tournament_file, indent=2)
+
 
