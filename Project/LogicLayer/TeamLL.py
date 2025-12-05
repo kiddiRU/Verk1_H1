ccaed6bafc47aa75bc3f0012f60b08ec47669c5d
Elmar-Sig <elmar.sigmars@gmail.com>
2025-12-05 12:04:10 +0000

    added to add player so that the input is the player handle, then gets the player uuid from the handle

--- Project/LogicLayer/TeamLL.py
+++ Project/LogicLayer/TeamLL.py
@@ -8,15 +8,27 @@ Created the TeamLL class and added the functions
 from Models.Exception import ValidationError
 from DataLayer import DataLayerAPI
 from Models.Team import Team
+from Models.Player import Player
+from LogicLayer.LogicUtility import get_player_uuid
 
 class TeamLL():
 
     def __init__(self) -> None:
         pass
 
+    
+    def get_team_uuid(self, player: Player) -> str:
+        player_uuid = player.uuid
+        model_teams: list = DataLayerAPI.load_teams()
+
+        for team in model_teams:
+            if player_uuid in team.list_player_uuid:
+                self.team_uuid = team.uuid
 
 
-    def add_player(self, team_uuid: str, player_uuid: str) -> Team:
+
+
+    def add_player(self, team_uuid: str, player_handle) -> Team:
         """
         Takes in team uuid and a player,
         First looks through all teams to see if the player uuid is already in a team
@@ -24,17 +36,18 @@ class TeamLL():
         finds the right team uuid and
         adds the new player uuid to the teams player list 
         """
+        player_uuid: str = get_player_uuid(player_handle)
         model_teams: list = DataLayerAPI.load_teams()
-
+        
         for team in model_teams:
             if player_uuid in team.list_player_uuid:
-                print("Player is already in a team")
+                raise ValidationError("Player is already in team")
 
             else:
                 for team in model_teams:            
                     if team.uuid == team_uuid:
                         if len(team.list_player_uuid) == 5:
-                            continue
+                            raise ValidationError("Max player count in team: 5")
 
                         else:
                             team.list_player_uuid.append(player_uuid)
@@ -55,7 +68,7 @@ class TeamLL():
         for team in model_teams:
             if team.uuid == team_uuid:
                 if player_uuid == team.team_captain_uuid:
-                    print("Cant remove the team captain")
+                    raise ValidationError("Can't remove team captain from team")
 
                 else:
                     try:
