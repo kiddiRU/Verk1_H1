487f277b225392c99c4c46f2c7e528ab3cf6ed6a
Elmar-Sig <elmar.sigmars@gmail.com>
2025-12-05 12:01:18 +0000

    improved the get player uuid (added if no player is found)

--- Project/LogicLayer/LogicUtility.py
+++ Project/LogicLayer/LogicUtility.py
@@ -6,16 +6,19 @@ Takes in handles and names of teams, and gets the uuid of them
 """
 
 from DataLayer import DataLayerAPI
+from Models.Exception import ValidationError
 
 
-def get_player_uuid(self, player_handle):
+def get_player_uuid(player_handle) -> str:
     """
     Takes in player handle
     looks through all players until it finds the right player
     and returns the player uuid
+    If no player is found an error is raised
     """
     model_players: list = DataLayerAPI.load_players()
-
     for player in model_players:
         if player_handle == player.handle:
             return player.uuid
+        
+    raise ValidationError("Player not found")
