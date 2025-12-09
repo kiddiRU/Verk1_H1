9f252a9584133d16a499fc95e4fa1af0c0da10a6
Elmar-Sig <elmar.sigmars@gmail.com>
2025-12-09 13:37:31 +0000

    added and implemented change match winner

--- Project/LogicLayer/MatchLL.py
+++ Project/LogicLayer/MatchLL.py
@@ -7,6 +7,7 @@ Functions for Match logic
 
 from uuid import uuid4
 from Models.Match import Match
+from Models.Team import Team
 from DataLayer import DataLayerAPI
 from datetime import date,time
 
@@ -17,7 +18,10 @@ class MatchLL():
 
     
     def create_match(self, tournament_id: str, date: date, time: time, team_1: str, team_2: str) -> Match:
-
+        """
+        takes in tournament id, date, time, team_1 and team_2
+        and creates a new match using the info
+        """
         uuid = str(uuid4())
         new_match = Match(uuid, tournament_id, date, time, team_1, team_2)
         DataLayerAPI.store_match(new_match)
@@ -25,6 +29,11 @@ class MatchLL():
 
 
     def get_matches(self, tournament_id: str) -> list[Match]:
+        """
+        takes in a tournament uuid
+        and finds all matches in the tournament
+        and returns list of Match object sorted by their time
+        """
         model_matches: list[Match] = DataLayerAPI.load_matches()
         model_matches = [
             match
@@ -39,11 +48,45 @@ class MatchLL():
 
 
     def change_match_winner(self, match_uuid: str, team_uuid: str) -> Match:
+        """
+        takes in match uuid and team uuid of the winning team
+        updates the info of a match
+        changes the winning and losing team
+        and adds a list of all the players in the teams
+        """
         model_matches: list[Match] = DataLayerAPI.load_matches()
+        model_teams: list[Team] = DataLayerAPI.load_teams()
 
         for match in model_matches:
+            
+            # stores the winning team
             if match.uuid == match_uuid and match.winner is None:
                 match.winner = team_uuid
+
+                for w_team in model_teams:
+
+                    # stores the winning players
+                    if w_team.uuid == team_uuid:
+                        winning_players: list[str] = w_team.list_player_uuid
+                        match.winning_players = winning_players
+            
+
+                # Checks which of the teams is the loser
+                if team_uuid != match.team_1:
+                    losing_team = match.team_1
+
+                else:
+                    losing_team = match.team_2
+                
+                match.losing_team = losing_team
+                # Looks through all teams to find the loser team
+                print(f"Losing Team id: {losing_team}")
+                for l_team in model_teams:
+                    if l_team.uuid == losing_team:
+                        # Takes a list of the players on the losing team
+                        losing_players: list[str] = l_team.list_player_uuid
+                        match.losing_players = losing_players
+                        
                 DataLayerAPI.store_match(match)
 
                 return match
