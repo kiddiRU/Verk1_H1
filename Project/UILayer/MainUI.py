ba8d2207ee1b6ff8ffb933fc1152fcd1d1d5f341
IsakEliH <isakelihauksson@gmail.com>
2025-12-09 09:29:45 +0000

    removed clear in MainUI

--- Project/UILayer/MainUI.py
+++ Project/UILayer/MainUI.py
@@ -88,7 +88,6 @@ class MainUI:
         """Main navigation loop"""
 
         while True:
-            self.__clear()
 
             if self.screens.get(self.current_screen) is not None:
                 self.current_screen = self.screens[self.current_screen]()
