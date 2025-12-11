f49a72e0579eb67edc9466c9b17ca2e2b86e8a77
kristjanhj <kristjanhagalin@gmail.com>
2025-12-11 17:53:26 +0000

    Docstrings, comments, pylint

--- Project/LogicLayer/PlayerLL.py
+++ Project/LogicLayer/PlayerLL.py
@@ -9,7 +9,7 @@ Functions for player logic.
 
 from uuid import uuid4
 from DataLayer import DataLayerAPI
-from Models import Player, Team, Match, Tournament, ValidationError
+from Models import Player, Team, Match, Tournament
 from LogicLayer import MatchLL, TeamLL
 
 class PlayerLL:
@@ -72,8 +72,10 @@ class PlayerLL:
             A new Player object.
         :rtype: Player
         '''
+        # Create a unique UUID.
         uuid = str(uuid4())
 
+        # Create the new Player object.
         new_player = Player(
             uuid,
             name,
@@ -85,6 +87,7 @@ class PlayerLL:
             url
         )
 
+        # Store and return the new player.
         DataLayerAPI.store_player(new_player)
         return new_player
 
@@ -134,6 +137,8 @@ class PlayerLL:
             An updated Player object.
         :rtype: Player
         '''
+
+        # Set the attributes of the Player object received.
         player.name = name
         player.date_of_birth = date_of_birth
         player.home_address = home_address
@@ -142,6 +147,7 @@ class PlayerLL:
         player.handle = handle
         player.url = url
 
+        # Store and return the updated player.
         DataLayerAPI.update_player(player.uuid, player)
         return player
 
@@ -165,41 +171,42 @@ class PlayerLL:
     #     team.list_player_uuid.remove(player.uuid)
     #     DataLayerAPI.update_team(team.uuid, team)
 
-    def promote_captain(self, current_player: Player, handle_to_promote: str) -> None:
-        '''Promotes a teams player to its captain.
-        
-        :param current_player:
-            The object of the player calling the function.
-        :type current_player: Player
+    # Isn't used, remove?
+    # def promote_captain(self, current_player: Player, handle_to_promote: str) -> None:
+    #     '''Promotes a teams player to its captain.
 
-        :param handle_to_promote:
-            The handle of the player to promote.
-        :type handle_to_promote: str
-        '''
-        team_to_edit = next(
-            (t for t in DataLayerAPI.load_teams() if current_player.uuid == t.team_captain_uuid),
-            None
-        )
+    #     :param current_player:
+    #         The object of the player calling the function.
+    #     :type current_player: Player
 
-        if team_to_edit is None:
-            raise ValidationError('You are not a captain!')
+    #     :param handle_to_promote:
+    #         The handle of the player to promote.
+    #     :type handle_to_promote: str
+    #     '''
+    #     team_to_edit = next(
+    #         (t for t in DataLayerAPI.load_teams() if current_player.uuid == t.team_captain_uuid),
+    #         None
+    #     )
 
-        players: list[Player] = DataLayerAPI.load_players()
-        player_to_promote: Player | None = next(
-            (p for p in players if p.handle == handle_to_promote),
-            None
-        )
+    #     if team_to_edit is None:
+    #         raise ValidationError('You are not a captain!')
 
-        if player_to_promote is None:
-            raise ValidationError(f'No player found with the handle: {handle_to_promote}')
+    #     players: list[Player] = DataLayerAPI.load_players()
+    #     player_to_promote: Player | None = next(
+    #         (p for p in players if p.handle == handle_to_promote),
+    #         None
+    #     )
 
-        if player_to_promote.uuid not in team_to_edit.list_player_uuid:
-            raise ValidationError(
-                f'The player \'{handle_to_promote}\' exists, but is not in your team!'
-            )
+    #     if player_to_promote is None:
+    #         raise ValidationError(f'No player found with the handle: {handle_to_promote}')
 
-        team_to_edit.team_captain_uuid = player_to_promote.uuid
-        DataLayerAPI.update_team(team_to_edit.uuid, team_to_edit)
+    #     if player_to_promote.uuid not in team_to_edit.list_player_uuid:
+    #         raise ValidationError(
+    #             f'The player \'{handle_to_promote}\' exists, but is not in your team!'
+    #         )
+
+    #     team_to_edit.team_captain_uuid = player_to_promote.uuid
+    #     DataLayerAPI.update_team(team_to_edit.uuid, team_to_edit)
 
     def save_player(self, player_handle: str | None = None):
         """ Takes in a player handle and saves them as the current active user. """
@@ -210,20 +217,36 @@ class PlayerLL:
         return self.player
 
     def get_player_team_and_rank(self, player_handle: str) -> tuple[str, str]:
-        """Takes in a player handle and returns the name of their team and their rank"""
+        '''Gets a players team and rank.
+        
+        :param player_handle:
+            The players unique handle.
+        :type player_handle: str
 
-        player_uuid: Player | str = self.player_handle_to_uuid(player_handle)
+        :return:
+            Returns a tuple of the players team name and their rank.
+        :rtype: tuple[str, str]
+        '''
+        # Get a list of all teams, and the players UUID.
         teams: list[Team] = self._team_logic.list_all_teams()
+        player_uuid: Player | str = self.player_handle_to_uuid(player_handle)
 
-        for team in teams:
-            players = self._team_logic.get_team_members(team.name)
+        # Get the players team.
+        players_team: Team | None = next(
+            (t for t in teams if player_uuid in t.list_player_uuid),
+            None
+        )
 
-            if player_uuid in players:
-                if team.team_captain_uuid == player_uuid:
-                    return team.name, "Captain"
-                return team.name, "Player"
+        # If the player is in no team, return an empty tuple.
+        if players_team is None:
+            return ('', '')
 
-        return ("","") # This will never be returned (it is just to appease the type hinting gods)
+        # Return a tuple of the players team and rank.
+        return (
+            players_team.name, 'Captain'
+        ) if players_team.team_captain_uuid == player_uuid else (
+            players_team.name, 'Player'
+        )
 
     def get_player_wins(self, player_handle: str) -> str:
         """Gets the player handle
@@ -281,7 +304,6 @@ class PlayerLL:
 
         # Loops through all tournaments
         for tournament in model_tournaments:
-            
             matches_list: list[Match] = self._match_logic.get_matches(tournament.uuid)
 
             # if matches is empty skips tournaments
@@ -307,8 +329,6 @@ class PlayerLL:
             elif player_uuid in losing_players:
                 points += 1
 
-
-
         return str(points)
 
     def list_all_players(self) -> list[Player]:
@@ -317,7 +337,7 @@ class PlayerLL:
         :return: Returns a list of player objects
         :rtype: list[Player]
         """
-
+        # Get a list of all stored players as objects, and return them.
         players: list[Player] = DataLayerAPI.load_players()
         return players
 
@@ -332,9 +352,11 @@ class PlayerLL:
             if the player isn't found.
         :rtype: Player | str
         '''
+        # Get list of all stored players, and find the player with the given handle.
         players: list[Player] = self.list_all_players()
         player = next((p for p in players if p.handle == player_handle), None)
 
+        # If the no player exists with the given handle, return an empty string.
         if player is None:
             return ""
 
@@ -352,9 +374,11 @@ class PlayerLL:
             if the player isn't found.
         :rtype: Player | str
         '''
+        # Get list of all stored players, and find the player with the given UUID.
         players: list[Player] = self.list_all_players()
         player: Player | None = next((p for p in players if p.uuid == player_uuid), None)
 
+        # If the no player exists with the given UUID, return an empty string.
         if player is None:
             return ""
 
@@ -371,6 +395,8 @@ class PlayerLL:
             Returns the UUID of the player with the given handle.
         :rtype: str
         '''
+        # Get the Player object with the given handle. Return its UUID
+        # if it exists, else an empty string.
         player: Player | str = self.get_player_by_handle(player_handle)
         return player.uuid if isinstance(player, Player) else ''
 
@@ -388,8 +414,12 @@ class PlayerLL:
         :return: Returns a teams uuid
         :rtype: str
         """
-        model_teams: list[Team] = DataLayerAPI.load_teams()
-        for team in model_teams:
+        # Get all teams
+        teams: list[Team] = DataLayerAPI.load_teams()
+
+        # If the given players UUID is found
+        # in the team, return the teams UUID.
+        for team in teams:
             if player_uuid in team.list_player_uuid:
                 return team.uuid
 
